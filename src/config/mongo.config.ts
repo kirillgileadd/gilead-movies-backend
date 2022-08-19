26fa67e9aaba10e3980810a2fed37f9a92490059
kirillgileadd <kirillgilead@gmail.com>
2022-08-19 17:48:55 +0300

    deploy

--- src/config/mongo.config.ts
+++ src/config/mongo.config.ts
@@ -6,6 +6,6 @@ const options = {}
 export const getMongoConfig = async (
 	configService: ConfigService
 ): Promise<TypegooseModuleOptions> => ({
-	uri: configService.get('MONGO_URI' || 'mongodb+srv://root:root@cluster0.bw2lu4r.mongodb.net/?retryWrites=true&w=majority'),
+	uri: configService.get('MONGO_URI' || 'mongodb+srv://root:root@clustercovid-ooazl.mongodb.net/?retryWrites=true&w=majority'),
 	...options,
 })
